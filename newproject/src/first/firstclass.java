b7ee20d19ad76d240a956896241b915b8dec8b20
kache <kache@LAPTOP-E04GE1N8>
2022-08-26 22:27:08 +0530

    jira id change

--- newproject/src/first/firstclass.java
+++ newproject/src/first/firstclass.java
@@ -3,5 +3,7 @@ package first;
 public class firstclass {
      public static void main(String[] args) {
 		System.out.println("my first programme");
+		System.out.println("12345");
+		System.out.println("changes done");
 	}
 }
